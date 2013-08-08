5f63731f8a5f7b49073f39c4e5f59f390236be1b
arin <arindpfmsls@gmail.com>
2013-08-09 07:51:22 +0900

    늑대인간 수정
    늑대인간 퀘스트 일부 수정
    늑대인간 패시브 스킬 시작부터 가지도록 수정

---
+++ sql/Story_updates/13_08_09_01_playercreateinfo_spell.sql
@@ -0,0 +1,8 @@
+-- 늑대인간 패시브 시작부터 가지도록 수정
+UPDATE `playercreateinfo_spell` SET `classmask`=0 WHERE  `racemask`=2097152 AND `classmask`=32 AND `Spell`=87840;
+UPDATE `playercreateinfo_spell` SET `classmask`=0 WHERE  `racemask`=2097152 AND `classmask`=32 AND `Spell`=68996;
+UPDATE `playercreateinfo_spell` SET `classmask`=0 WHERE  `racemask`=2097152 AND `classmask`=32 AND `Spell`=68975;
+UPDATE `playercreateinfo_spell` SET `classmask`=0 WHERE  `racemask`=2097152 AND `classmask`=32 AND `Spell`=94293;
+UPDATE `playercreateinfo_spell` SET `classmask`=0 WHERE  `racemask`=2097152 AND `classmask`=32 AND `Spell`=68992;
+UPDATE `playercreateinfo_spell` SET `classmask`=0 WHERE  `racemask`=2097152 AND `classmask`=32 AND `Spell`=68976;
+UPDATE `playercreateinfo_spell` SET `classmask`=509 WHERE  `racemask`=256 AND `classmask`=32 AND `Spell`=69046;
